a62fd06f721edd16b3736aaaef8c7c055d401cec
Shaul Hameed <shaul21h@gmail.com>
2017-04-17 13:27:13 +0530

    initial commit

---
+++ Sweetner/Sweetner.h
@@ -0,0 +1,19 @@
+//
+//  Sweetner.h
+//  Sweetner
+//
+//  Created by Shaul Hameed on 17/04/17.
+//  Copyright © 2017 Shaul hameed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sweetner.
+FOUNDATION_EXPORT double SweetnerVersionNumber;
+
+//! Project version string for Sweetner.
+FOUNDATION_EXPORT const unsigned char SweetnerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sweetner/PublicHeader.h>
+
+
---
+++ Sweetner/Sweetner.swift
@@ -0,0 +1,23 @@
+//
+//  Sweetner.swift
+//  Sweetner
+//
+//  Created by Shaul Hameed on 17/04/17.
+//  Copyright © 2017 Shaul hameed. All rights reserved.
+//
+
+import UIKit
+
+class Sweetner: NSObject {
+    
+    override init(){
+        
+        super.init()
+        
+    }
+    
+    
+    func isFound(){
+        debugPrint("hello")
+    }
+}
